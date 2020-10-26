dc1729b9fd91bc099df57e833fe8fa206f981caf
Adrian Vivas <adrian.vivas@areamovil.com.co>
2020-10-25 23:47:47 -0500

    5 comiit

--- src/main/java/com/ceiba/tiendatecnologica/infraestructura/error/ManejadorError.java
+++ src/main/java/com/ceiba/tiendatecnologica/infraestructura/error/ManejadorError.java
@@ -13,12 +13,13 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ManejadorError extends ResponseEntityExceptionHandler {
 
     private static final String OCURRIO_UN_ERROR_FAVOR_CONTACTAR_AL_ADMINISTRADOR = "Ocurrió un error favor contactar al administrador.";
-    
+
     private static final ConcurrentHashMap<String, Integer> CODIGOS_ESTADO = new ConcurrentHashMap<>();
 
     public ManejadorError() {
         CODIGOS_ESTADO.put(GarantiaExtendidaException.class.getSimpleName(), HttpStatus.BAD_REQUEST.value());
         //en caso de tener otra excepcion matricularla aca
+
     }
 
     @ExceptionHandler(Exception.class)
